747fe92d7b523b30d90a69d8393e48a38afa377d
Dhrunit <jimmyprajapati99@gmail.com>
2020-12-25 13:58:55 +0530

    Added a useParams hook to get userid from url and also added a button component

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '../../shared/components/FormElements/Button'
 
 import Card from '../../shared/components/UIElements/Card'
 import './PlaceItem.css'
@@ -15,9 +16,9 @@ export default function PlaceItem(props) {
 					<p>{props.description}</p>
 				</div>
 				<div className='place-item__actions'>
-					<button>View on map</button>
-					<button>Edit</button>
-					<button>Delete</button>
+					<Button inverse>View on Map</Button>
+					<Button to={`/places/${props.id}`}>Edit</Button>
+					<Button danger>Delete</Button>
 				</div>
 			</Card>
 		</li>
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 import PlaceList from '../components/PlaceList'
 
@@ -32,9 +33,13 @@ const DUMMY_PLACES = [
 ]
 
 export default function UserPlaces() {
+	const userId = useParams().userId
+	const loadedPlaces = DUMMY_PLACES.filter(
+		(place) => place.creator === userId
+	)
 	return (
 		<div>
-			<PlaceList items={DUMMY_PLACES} />
+			<PlaceList items={loadedPlaces} />
 		</div>
 	)
 }
