df79222a52ed45e351150f4470bcbddd46aab01f
Dhrunit <jimmyprajapati99@gmail.com>
2020-12-27 13:45:28 +0530

    Added a new util function to validator prop for validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,5 +1,19 @@
 import React from 'react'
 
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators'
+import Input from '../../shared/components/FormElements/Input'
+import './NewPlace.css'
 export default function NewPlace() {
-	return <div>New Place works</div>
+	return (
+		<form className='place-form'>
+			<Input
+				element='input'
+				type='text'
+				label='Title'
+				validators={[VALIDATOR_REQUIRE()]}
+				onInput={[]}
+				errorText='Please enter a valid Title'
+			/>
+		</form>
+	)
 }
