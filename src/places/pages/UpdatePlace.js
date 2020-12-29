17eb92e3335e22c348d5a411a12ebe31ec686452
Dhrunit <jimmyprajapati99@gmail.com>
2020-12-29 08:01:28 +0530

    Now input uses predefined data and added a UpdatePlace file for updating a place

---
+++ src/places/pages/UpdatePlace.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import Input from '../../shared/components/FormElements/Input'
+import Button from '../../shared/components/FormElements/Button'
+import {
+	VALIDATOR_MINLENGTH,
+	VALIDATOR_REQUIRE,
+} from '../../shared/util/validators'
+import './PlaceForm.css'
+import { useForm } from '../../shared/hooks/form-hook'
+const DUMMY_PLACES = [
+	{
+		id: 'p1',
+		title: 'Empire State building',
+		description: 'Famous skyscraper',
+		imageUrl:
+			'https://newyorkyimby.com/wp-content/uploads/2020/09/DSCN0762-777x1036.jpg',
+		address: '20 W 34th St, New York, NY 10001, United States',
+		location: {
+			lat: 40.7484405,
+			lng: -73.9878584,
+		},
+		creator: 'u1',
+	},
+	{
+		id: 'p2',
+		title: 'Empire State building',
+		description: 'Famous skyscraper',
+		imageUrl:
+			'https://newyorkyimby.com/wp-content/uploads/2020/09/DSCN0762-777x1036.jpg',
+		address: '20 W 34th St, New York, NY 10001, United States',
+		location: {
+			lat: 40.7484405,
+			lng: -73.9878584,
+		},
+		creator: 'u2',
+	},
+]
+
+export default function UpdatePlace(props) {
+	const placeId = useParams().placeId
+	const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId)
+
+	const [formState, inputHandler] = useForm(
+		{
+			title: {
+				value: identifiedPlace.title,
+				isValid: true,
+			},
+			description: {
+				value: identifiedPlace.description,
+				isValid: true,
+			},
+		},
+		true
+	)
+	const placeUpdateSubmitHandler = (event) => {
+		event.preventDefault()
+		console.log(formState.inputs)
+	}
+
+	if (!identifiedPlace) {
+		return (
+			<div className='center'>
+				<h2>Could not find any places</h2>
+			</div>
+		)
+	}
+	return (
+		<form className='place-form' onSubmit={placeUpdateSubmitHandler}>
+			<Input
+				id='title'
+				element='input'
+				type='text'
+				label='Title'
+				validators={[VALIDATOR_REQUIRE()]}
+				errorText='Please enter a valid title.'
+				onInput={inputHandler}
+				initialValue={formState.inputs.title.value}
+				initialValid={formState.inputs.title.isValid}
+			/>
+			<Input
+				id='description'
+				element='textarea'
+				label='Description'
+				validators={[VALIDATOR_MINLENGTH(5)]}
+				errorText='Please enter a valid description (min. 5 characters).'
+				onInput={inputHandler}
+				initialValue={formState.inputs.description.value}
+				initialValid={formState.inputs.description.isValid}
+			/>
+			<Button type='submit' disabled={!formState.isValid}>
+				UPDATE PLACE
+			</Button>
+		</form>
+	)
+}
