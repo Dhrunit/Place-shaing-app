03108ea1c9db1675e3a6a0ca87c115aa00ef78f4
Dhrunit <jimmyprajapati99@gmail.com>
2020-12-29 08:31:53 +0530

    Added useeffect to avoid infinite callback

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,22 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 import Input from '../../shared/components/FormElements/Input'
+import Card from '../../shared/components/UIElements/Card'
 import Button from '../../shared/components/FormElements/Button'
 import {
-	VALIDATOR_MINLENGTH,
 	VALIDATOR_REQUIRE,
+	VALIDATOR_MINLENGTH,
 } from '../../shared/util/validators'
-import './PlaceForm.css'
 import { useForm } from '../../shared/hooks/form-hook'
+import './PlaceForm.css'
+
 const DUMMY_PLACES = [
 	{
 		id: 'p1',
-		title: 'Empire State building',
-		description: 'Famous skyscraper',
+		title: 'Empire State Building',
+		description: 'One of the most famous sky scrapers in the world!',
 		imageUrl:
-			'https://newyorkyimby.com/wp-content/uploads/2020/09/DSCN0762-777x1036.jpg',
-		address: '20 W 34th St, New York, NY 10001, United States',
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+		address: '20 W 34th St, New York, NY 10001',
 		location: {
 			lat: 40.7484405,
 			lng: -73.9878584,
@@ -25,11 +27,11 @@ const DUMMY_PLACES = [
 	},
 	{
 		id: 'p2',
-		title: 'Empire State building',
-		description: 'Famous skyscraper',
+		title: 'Empire State Building',
+		description: 'One of the most famous sky scrapers in the world!',
 		imageUrl:
-			'https://newyorkyimby.com/wp-content/uploads/2020/09/DSCN0762-777x1036.jpg',
-		address: '20 W 34th St, New York, NY 10001, United States',
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+		address: '20 W 34th St, New York, NY 10001',
 		location: {
 			lat: 40.7484405,
 			lng: -73.9878584,
@@ -38,23 +40,45 @@ const DUMMY_PLACES = [
 	},
 ]
 
-export default function UpdatePlace(props) {
+const UpdatePlace = () => {
+	const [isLoading, setIsLoading] = useState(true)
 	const placeId = useParams().placeId
-	const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId)
 
-	const [formState, inputHandler] = useForm(
+	const [formState, inputHandler, setFormData] = useForm(
 		{
 			title: {
-				value: identifiedPlace.title,
-				isValid: true,
+				value: '',
+				isValid: false,
 			},
 			description: {
-				value: identifiedPlace.description,
-				isValid: true,
+				value: '',
+				isValid: false,
 			},
 		},
-		true
+		false
 	)
+
+	const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId)
+
+	useEffect(() => {
+		if (identifiedPlace) {
+			setFormData(
+				{
+					title: {
+						value: identifiedPlace.title,
+						isValid: true,
+					},
+					description: {
+						value: identifiedPlace.description,
+						isValid: true,
+					},
+				},
+				true
+			)
+		}
+		setIsLoading(false)
+	}, [setFormData, identifiedPlace])
+
 	const placeUpdateSubmitHandler = (event) => {
 		event.preventDefault()
 		console.log(formState.inputs)
@@ -63,10 +87,21 @@ export default function UpdatePlace(props) {
 	if (!identifiedPlace) {
 		return (
 			<div className='center'>
-				<h2>Could not find any places</h2>
+				<Card>
+					<h2>Could not find place!</h2>
+				</Card>
 			</div>
 		)
 	}
+
+	if (isLoading) {
+		return (
+			<div className='center'>
+				<h2>Loading...</h2>
+			</div>
+		)
+	}
+
 	return (
 		<form className='place-form' onSubmit={placeUpdateSubmitHandler}>
 			<Input
@@ -96,3 +131,5 @@ export default function UpdatePlace(props) {
 		</form>
 	)
 }
+
+export default UpdatePlace
