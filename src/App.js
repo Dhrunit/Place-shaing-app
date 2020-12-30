1d9819036111ae791d168887595ec34547513d8c
Dhrunit <jimmyprajapati99@gmail.com>
2020-12-30 16:21:00 +0530

    Added dynamic routes based on isLoggedIn

--- src/App.js
+++ src/App.js
@@ -21,30 +21,47 @@ function App() {
 	const logout = useCallback(() => {
 		setIsLoggedIn(false)
 	}, [])
+
+	let routes
+	if (isLoggedIn) {
+		routes = (
+			<Switch>
+				<Route path='/' exact>
+					<Users />
+				</Route>
+				<Route path='/:userId/places' exact>
+					<UserPlaces />
+				</Route>
+				<Route path='/places/new' exact>
+					<NewPlace />
+				</Route>
+				<Route path='/places/:placeId' exact>
+					<UpdatePlace />
+				</Route>
+				<Redirect to='/' />
+			</Switch>
+		)
+	} else {
+		routes = (
+			<Switch>
+				<Route path='/' exact>
+					<Users />
+				</Route>
+				<Route path='/:userId/places' exact>
+					<UserPlaces />
+				</Route>
+				<Route path='/auth' exact>
+					<Auth />
+				</Route>
+				<Redirect to='/auth' />
+			</Switch>
+		)
+	}
 	return (
-		<AuthContext.Provider value={(isLoggedIn, login, logout)}>
+		<AuthContext.Provider value={{ isLoggedIn, login, logout }}>
 			<Router>
 				<MainNavigation />
-				<main>
-					<Switch>
-						<Route path='/' exact>
-							<Users />
-						</Route>
-						<Route path='/:userId/places' exact>
-							<UserPlaces />
-						</Route>
-						<Route path='/places/new' exact>
-							<NewPlace />
-						</Route>
-						<Route path='/places/:placeId' exact>
-							<UpdatePlace />
-						</Route>
-						<Route path='/auth' exact>
-							<Auth />
-						</Route>
-						<Redirect to='/' />
-					</Switch>
-				</main>
+				<main>{routes}</main>
 			</Router>
 		</AuthContext.Provider>
 	)
