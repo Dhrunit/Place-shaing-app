b1322e9397d004c4af8c0920116e71b309d33098
Dhrunit <jimmyprajapati99@gmail.com>
2021-01-06 12:35:38 +0530

    used error handling modal in auth.js using state

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -3,7 +3,7 @@ import React, { useState, useContext } from 'react'
 import Card from '../../shared/components/UIElements/Card'
 import Input from '../../shared/components/FormElements/Input'
 import Button from '../../shared/components/FormElements/Button'
-import Modal from '../../shared/components/UIElements/ErrorModal'
+import ErrorModal from '../../shared/components/UIElements/ErrorModal'
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
 import {
 	VALIDATOR_EMAIL,
@@ -60,11 +60,11 @@ const Auth = () => {
 
 	const authSubmitHandler = async (event) => {
 		event.preventDefault()
+
 		if (isLoginMode) {
 		} else {
 			try {
 				setIsLoading(true)
-				// setError()
 				const response = await fetch(
 					'http://localhost:5000/api/users/signup',
 					{
@@ -79,63 +79,72 @@ const Auth = () => {
 						}),
 					}
 				)
+
 				const responseData = await response.json()
+				if (!response.ok) {
+					throw new Error(responseData.message)
+				}
 				console.log(responseData)
 				setIsLoading(false)
-			} catch (error) {
+				auth.login()
+			} catch (err) {
 				setIsLoading(false)
 				setError(
-					error.message || 'Something went Wrong please try again'
+					err.message || 'Something went wrong, please try again.'
 				)
-				console.log(error)
 			}
 		}
-		auth.login()
+	}
+	const errorHandler = () => {
+		setError(null)
 	}
 
 	return (
-		<Card className='authentication'>
-			{isLoading && <LoadingSpinner asOverlay />}
-			<h2>Login Required</h2>
-			<hr />
-			<form onSubmit={authSubmitHandler}>
-				{!isLoginMode && (
+		<>
+			<ErrorModal error={error} onClear={errorHandler} />
+			<Card className='authentication'>
+				{isLoading && <LoadingSpinner asOverlay />}
+				<h2>Login Required</h2>
+				<hr />
+				<form onSubmit={authSubmitHandler}>
+					{!isLoginMode && (
+						<Input
+							element='input'
+							id='name'
+							type='text'
+							label='Your Name'
+							validators={[VALIDATOR_REQUIRE()]}
+							errorText='Please enter a name.'
+							onInput={inputHandler}
+						/>
+					)}
 					<Input
 						element='input'
-						id='name'
-						type='text'
-						label='Your Name'
-						validators={[VALIDATOR_REQUIRE()]}
-						errorText='Please enter a name.'
+						id='email'
+						type='email'
+						label='E-Mail'
+						validators={[VALIDATOR_EMAIL()]}
+						errorText='Please enter a valid email address.'
 						onInput={inputHandler}
 					/>
-				)}
-				<Input
-					element='input'
-					id='email'
-					type='email'
-					label='E-Mail'
-					validators={[VALIDATOR_EMAIL()]}
-					errorText='Please enter a valid email address.'
-					onInput={inputHandler}
-				/>
-				<Input
-					element='input'
-					id='password'
-					type='password'
-					label='Password'
-					validators={[VALIDATOR_MINLENGTH(5)]}
-					errorText='Please enter a valid password, at least 5 characters.'
-					onInput={inputHandler}
-				/>
-				<Button type='submit' disabled={!formState.isValid}>
-					{isLoginMode ? 'LOGIN' : 'SIGNUP'}
+					<Input
+						element='input'
+						id='password'
+						type='password'
+						label='Password'
+						validators={[VALIDATOR_MINLENGTH(5)]}
+						errorText='Please enter a valid password, at least 5 characters.'
+						onInput={inputHandler}
+					/>
+					<Button type='submit' disabled={!formState.isValid}>
+						{isLoginMode ? 'LOGIN' : 'SIGNUP'}
+					</Button>
+				</form>
+				<Button inverse onClick={switchModeHandler}>
+					SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
 				</Button>
-			</form>
-			<Button inverse onClick={switchModeHandler}>
-				SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
-			</Button>
-		</Card>
+			</Card>
+		</>
 	)
 }
 
