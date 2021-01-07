43c5b897b54f9c19729c85eb43c5df26d59ba62b
Dhrunit <jimmyprajapati99@gmail.com>
2021-01-07 08:37:14 +0530

    sent the userid once user is logged in

--- src/App.js
+++ src/App.js
@@ -15,11 +15,14 @@ import Auth from './user/pages/Auth'
 import { AuthContext } from './shared/context/auth-context'
 function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false)
-	const login = useCallback(() => {
+	const [userId, setUserId] = useState(false)
+	const login = useCallback((uid) => {
 		setIsLoggedIn(true)
+		setUserId(uid)
 	}, [])
 	const logout = useCallback(() => {
 		setIsLoggedIn(false)
+		setUserId(null)
 	}, [])
 
 	let routes
@@ -58,7 +61,7 @@ function App() {
 		)
 	}
 	return (
-		<AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+		<AuthContext.Provider value={{ isLoggedIn, login, logout, userId }}>
 			<Router>
 				<MainNavigation />
 				<main>{routes}</main>
