feaf98081f1af561b3f5479d0f0bad9bc2c3922a
Dhrunit <jimmyprajapati99@gmail.com>
2021-01-07 12:27:14 +0530

    Added delete places logic

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,33 +1,54 @@
 import React, { useState, useContext } from 'react'
 
+import Card from '../../shared/components/UIElements/Card'
 import Button from '../../shared/components/FormElements/Button'
 import Modal from '../../shared/components/UIElements/Modal'
-import Card from '../../shared/components/UIElements/Card'
 import Map from '../../shared/components/UIElements/Map'
-import './PlaceItem.css'
+import ErrorModal from '../../shared/components/UIElements/ErrorModal'
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
 import { AuthContext } from '../../shared/context/auth-context'
-export default function PlaceItem(props) {
+import { useHttpCLient } from '../../shared/hooks/http-hook'
+import './PlaceItem.css'
+
+const PlaceItem = (props) => {
+	const { isLoading, error, sendRequest, clearError } = useHttpCLient()
 	const auth = useContext(AuthContext)
 	const [showMap, setShowMap] = useState(false)
 	const [showConfirmModal, setShowConfirmModal] = useState(false)
 
 	const openMapHandler = () => setShowMap(true)
+
 	const closeMapHandler = () => setShowMap(false)
-	const showDeleteWarningHandler = () => setShowConfirmModal(true)
-	const cancelDeleteHandler = () => setShowConfirmModal(false)
-	const confirmDeleteHandler = () => {
-		setShowConfirmModal(false)
-		console.log('deleting')
+
+	const showDeleteWarningHandler = () => {
+		setShowConfirmModal(true)
 	}
+
+	const cancelDeleteHandler = () => {
+		setShowConfirmModal(false)
+	}
+
+	const confirmDeleteHandler = async () => {
+		setShowConfirmModal(false)
+		try {
+			await sendRequest(
+				`http://localhost:5000/api/places/${props.id}`,
+				'DELETE'
+			)
+			props.onDelete(props.id)
+		} catch (err) {}
+	}
+
 	return (
 		<React.Fragment>
+			<ErrorModal error={error} onClear={clearError} />
 			<Modal
 				show={showMap}
 				onCancel={closeMapHandler}
 				header={props.address}
 				contentClass='place-item__modal-content'
 				footerClass='place-item__modal-actions'
-				footer={<Button onClick={closeMapHandler}>Close</Button>}>
+				footer={<Button onClick={closeMapHandler}>CLOSE</Button>}>
 				<div className='map-container'>
 					<Map center={props.coordinates} zoom={16} />
 				</div>
@@ -35,22 +56,26 @@ export default function PlaceItem(props) {
 			<Modal
 				show={showConfirmModal}
 				onCancel={cancelDeleteHandler}
-				header='Are you sure ?'
+				header='Are you sure?'
 				footerClass='place-item__modal-actions'
 				footer={
-					<>
+					<React.Fragment>
 						<Button inverse onClick={cancelDeleteHandler}>
-							Cancel
+							CANCEL
 						</Button>
 						<Button danger onClick={confirmDeleteHandler}>
-							Delete
+							DELETE
 						</Button>
-					</>
+					</React.Fragment>
 				}>
-				<p>Do you want to proceed and delete this place ?</p>
+				<p>
+					Do you want to proceed and delete this place? Please note
+					that it can't be undone thereafter.
+				</p>
 			</Modal>
 			<li className='place-item'>
 				<Card className='place-item__content'>
+					{isLoading && <LoadingSpinner asOverlay />}
 					<div className='place-item__image'>
 						<img src={props.image} alt={props.title} />
 					</div>
@@ -61,14 +86,15 @@ export default function PlaceItem(props) {
 					</div>
 					<div className='place-item__actions'>
 						<Button inverse onClick={openMapHandler}>
-							View on Map
+							VIEW ON MAP
 						</Button>
-						{auth.isLoggedIn && (
-							<Button to={`/places/${props.id}`}>Edit</Button>
+						{auth.userId === props.creatorId && (
+							<Button to={`/places/${props.id}`}>EDIT</Button>
 						)}
-						{auth.isLoggedIn && (
+
+						{auth.userId === props.creatorId && (
 							<Button danger onClick={showDeleteWarningHandler}>
-								Delete
+								DELETE
 							</Button>
 						)}
 					</div>
@@ -77,3 +103,5 @@ export default function PlaceItem(props) {
 		</React.Fragment>
 	)
 }
+
+export default PlaceItem
