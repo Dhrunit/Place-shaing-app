f1a9b600236f1f438b702214f6b1680ac384e194
Dhrunit <jimmyprajapati99@gmail.com>
2021-01-07 12:28:37 +0530

    Added a onDeletePlace prop for PlaceList

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+
+import PlaceList from '../components/PlaceList'
 import ErrorModal from '../../shared/components/UIElements/ErrorModal'
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
-import PlaceList from '../components/PlaceList'
 import { useHttpCLient } from '../../shared/hooks/http-hook'
-export default function UserPlaces() {
-	const [loadedPlaces, setIsLoadedPlaces] = useState()
+
+const UserPlaces = () => {
+	const [loadedPlaces, setLoadedPlaces] = useState()
 	const { isLoading, error, sendRequest, clearError } = useHttpCLient()
+
 	const userId = useParams().userId
 
 	useEffect(() => {
@@ -15,21 +18,34 @@ export default function UserPlaces() {
 				const responseData = await sendRequest(
 					`http://localhost:5000/api/places/user/${userId}`
 				)
-				setIsLoadedPlaces(responseData.places)
-			} catch (error) {}
+				setLoadedPlaces(responseData.places)
+			} catch (err) {}
 		}
 		fetchPlaces()
 	}, [sendRequest, userId])
 
+	const placeDeletedHandler = (deletedPlaceId) => {
+		setLoadedPlaces((prevPlaces) =>
+			prevPlaces.filter((place) => place.id !== deletedPlaceId)
+		)
+	}
+
 	return (
-		<>
+		<React.Fragment>
 			<ErrorModal error={error} onClear={clearError} />
 			{isLoading && (
 				<div className='center'>
-					<LoadingSpinner asOverlay />
+					<LoadingSpinner />
 				</div>
 			)}
-			{!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
-		</>
+			{!isLoading && loadedPlaces && (
+				<PlaceList
+					items={loadedPlaces}
+					onDeletePlace={placeDeletedHandler}
+				/>
+			)}
+		</React.Fragment>
 	)
 }
+
+export default UserPlaces
