1d5eeadce8eff9d5a9f793777d398d90c458cb78
Dhrunit <jimmyprajapati99@gmail.com>
2021-01-07 12:28:08 +0530

    added a on delete prop for placeitem.js

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -4,17 +4,19 @@ import Card from '../../shared/components/UIElements/Card'
 import PlaceItem from './PlaceItem'
 import Button from '../../shared/components/FormElements/Button'
 import './PlaceList.css'
-export default function PlaceList(props) {
+
+const PlaceList = (props) => {
 	if (props.items.length === 0) {
 		return (
 			<div className='place-list center'>
 				<Card>
-					<h2>No places found, Maybe create one ?</h2>
+					<h2>No places found. Maybe create one?</h2>
 					<Button to='/places/new'>Share Place</Button>
 				</Card>
 			</div>
 		)
 	}
+
 	return (
 		<ul className='place-list'>
 			{props.items.map((place) => (
@@ -27,8 +29,11 @@ export default function PlaceList(props) {
 					address={place.address}
 					creatorId={place.creator}
 					coordinates={place.location}
+					onDelete={props.onDeletePlace}
 				/>
 			))}
 		</ul>
 	)
 }
+
+export default PlaceList
