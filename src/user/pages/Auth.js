0bb6596f12a458307d1127453fe95ea401ffab99
Dhrunit <jimmyprajapati99@gmail.com>
2021-01-08 12:29:07 +0530

    Added the image upload logic to the frontend

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -85,17 +85,18 @@ const Auth = () => {
 			} catch (err) {}
 		} else {
 			try {
+				// Added multer configuration in the frontend we used FormData here which handles all type of data
+				// we append the values to the object and removed the headers because it automatically sets the data
+				const formData = new FormData()
+				formData.append('name', formState.inputs.name.value)
+				formData.append('email', formState.inputs.email.value)
+				formData.append('password', formState.inputs.password.value)
+				formData.append('image', formState.inputs.image.value)
+
 				const responseData = await sendRequest(
 					'http://localhost:5000/api/users/signup',
 					'POST',
-					JSON.stringify({
-						name: formState.inputs.name.value,
-						email: formState.inputs.email.value,
-						password: formState.inputs.password.value,
-					}),
-					{
-						'Content-Type': 'application/json',
-					}
+					formData
 				)
 
 				auth.login(responseData.user.id)
